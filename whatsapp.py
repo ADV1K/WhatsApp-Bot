55092b29cdc4e437d36e5f8260a06177d86b11f8
Advik <35737096+ADV1K@users.noreply.github.com>
2020-03-27 15:48:46 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -29,9 +29,9 @@ class WhastsApp:
 		options = Options()
 		options.headless = headless
 		# user agent is required for headless chrome to work properly.
-		options.add_argument("user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36")
+		options.add_argument("--user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36")
 		# options.add_argument("--window-size=1366x768")
-		#options.add_argument("--user-data")
+		#options.add_argument("--user-data-dir=whatsapp-data-dir")  # saves the whatsapp login so you dont have to login everytime 
 		
 		chrome_driver = os.path.join(os.getcwd(), "chromedriver.exe")
 
